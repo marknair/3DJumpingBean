6ba107936a59aa2a2912691efc49565832eaf67a
Mark Nair <mark@TRM-TBD.AscensionAcademy.local>
2025-11-07 09:37:57 -0600

    First commit

---
+++ Scripts/player.gd
@@ -0,0 +1,24 @@
+extends CharacterBody3D
+
+@export var move_speed : float = 3.0
+@export var jump_force : float = 8.0
+@export var gravity : float = 20.0
+
+@onready var camera : Camera3D = $Camera3D
+
+func _physics_process(delta: float) -> void:
+	# Apply gravity
+	velocity.y -= gravity * delta
+	
+	# Implement jumping
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force
+	
+	# Implement movement
+	var move_input : Vector2 = -Input.get_vector("move_right", "move_left", "more_back", "more_forward")
+	var move_dir : Vector3 = Vector3(move_input.x, 0, move_input.y)
+	velocity.x = move_dir.x * move_speed
+	velocity.z = move_dir.z * move_speed
+	
+	
+	move_and_slide()
